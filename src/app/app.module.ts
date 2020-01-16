c0a4f58c1f5b155bbbdd0d6e772314c82d499502
ellebanna <3.29.99annabelle@gmail.com>
2020-01-16 08:52:08 +0800

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ThroughCliComponent } from './through-cli/through-cli.component';
 import { ManuallyComponent } from './manually/manual';
 import { BindingComponent } from './binding/binding.component';
+import { ParentComponent } from './parent/parent';
 
 @NgModule({
   declarations: [
     AppComponent,
     ThroughCliComponent,
     ManuallyComponent,
-    BindingComponent
+    BindingComponent,
+    ParentComponent,
+    FormsModule
   ],
   imports: [
     BrowserModule,
